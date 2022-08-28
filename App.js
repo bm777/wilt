f7a92ffb2ba9b4149d7911bf57bea85ba819046c
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-28 05:09:24 +0100

    Added tailwindcss

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native'
+import { TailwindProvider } from 'tailwindcss-react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world around bayang</Text>
+    <TailwindProvider>
+      <Text className="text-amber-700">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </TailwindProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
---
+++ components/CustomButton.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+
+export default function Btn() {
+
+    return (
+      <View>
+        <Text>CustomButton</Text>
+      </View>
+    )
+
+}
