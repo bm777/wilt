5cadb4a97ad04d2dbd38c7bd313df5b53efdcbd7
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-28 07:07:35 +0100

    Added Tab navigator

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+function Ticket({ navigation }){
+  return (
+    <TailwindProvider>
+      <View className="flex-1 items-center justify-center">
+        <Text >Ticket</Text>
+        
+      </View>
+    </TailwindProvider> 
+  )
+}
+function Profile({ navigation }){
+  return (
+    <TailwindProvider>
+      <View className="flex-1 items-center justify-center">
+        <Text>Profile</Text>
+        
+      </View>
+      
+    </TailwindProvider> 
+  )
+}
+
+// the Bottom Navigator
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <Text className="text-amber-700">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </TailwindProvider>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({tabBarIcon: ({focused, color, size}) => {
+          let iconName
+          if (route.name === "Ticket"){
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline"
+          } else if (route.name === "Profile") {
+            iconName = focused ? "ios-list-box" : "ios-list"
+          }
+          return <Ionicons name={iconName} size={size} color={color}/>
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray"
+        })}
+      >
+        <Tab.Screen name='Ticket' component={Ticket}></Tab.Screen>
+        <Tab.Screen name='Profile' component={Profile}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
