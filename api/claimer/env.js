02e923d1773b0a7c44645913307b6f3efc042df4
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-30 07:51:20 +0100

    removed .js file which calls Polkadot and kiltprotocol

---
+++ api/claimer/env.js
@@ -0,0 +1,3 @@
+export const WSS_ADDRESS = "wss://peregrine.kilt.io/parachain-public-ws"
+export const MONGODB_URI = "mongodb+srv://bm777:Password777@cluster0.jfnuu9f.mongodb.net/wilt?retryWrites=true&w=majority"
+export const DB = "wilt"
