f76c4ede62e709127788d5b416b2978c49f30b3f
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-09-01 09:56:46 +0100

    fixing bigInt issue

--- components/Profile.js
+++ components/Profile.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Button, Text, TextInput, View } from 'react-native'
 import { TailwindProvider } from 'tailwindcss-react-native';
-
+import {init, Did} from "@kiltprotocol/sdk-js"
 
 const  Profile = ({ navigation }) => {
     // text
